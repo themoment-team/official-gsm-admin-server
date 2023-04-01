fe6460f790e7f55bf20a9affaa5698c9bccb866e
jangwooooo <s22018@gsm.hs.kr>
2023-04-01 18:49:29 +0900

    Add Post도메인

---
+++ src/main/java/com/themoment/officialgsm/domain/post/domain/Post.java
@@ -0,0 +1,26 @@
+package com.themoment.officialgsm.domain.post.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postSeq;
+    private String postTitle;
+    private String postContent;
+    private String postWriter;
+
+}
