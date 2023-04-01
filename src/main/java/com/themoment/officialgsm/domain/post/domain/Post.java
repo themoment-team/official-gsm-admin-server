2e0de750c04e5a63cb7266d0dd58127e113c06f2
jangwooooo <s22018@gsm.hs.kr>
2023-04-01 18:57:37 +0900

    Add BaseTimeEntity(생성일시, 수정일시)

--- src/main/java/com/themoment/officialgsm/domain/post/domain/Post.java
+++ src/main/java/com/themoment/officialgsm/domain/post/domain/Post.java
@@ -1,5 +1,6 @@
 package com.themoment.officialgsm.domain.post.domain;
 
+import com.themoment.officialgsm.global.entity.BaseTimeEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Post {
+public class Post extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
