a223d21cc7351ddbef6f590a4add04994ea14e6e
jangwooooo <s22018@gsm.hs.kr>
2023-04-03 09:26:26 +0900

    Add File엔티티

---
+++ src/main/java/com/themoment/officialgsm/domain/post/domain/File.java
@@ -0,0 +1,25 @@
+package com.themoment.officialgsm.domain.post.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long fileSeq;
+
+    private String fileUri;
+
+}
