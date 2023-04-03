ba23758bb045944cd609f025ef804baa9fce3a23
jangwooooo <s22018@gsm.hs.kr>
2023-04-03 11:52:13 +0900

    Add 파일타입을 담는 enum클래스 Type

--- src/main/java/com/themoment/officialgsm/domain/post/domain/File.java
+++ src/main/java/com/themoment/officialgsm/domain/post/domain/File.java
@@ -1,9 +1,6 @@
 package com.themoment.officialgsm.domain.post.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,4 +19,6 @@ public class File {
 
     private String fileUri;
 
+    @Enumerated(EnumType.STRING)
+    private Type type;
 }
---
+++ src/main/java/com/themoment/officialgsm/domain/post/domain/Type.java
@@ -0,0 +1,12 @@
+package com.themoment.officialgsm.domain.post.domain;
+
+public enum Type {
+
+    PNG,
+    JPG,
+    MP4,
+    HWP,
+    PDF,
+    XLSX
+
+}
