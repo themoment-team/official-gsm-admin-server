f5597886b579db62971249b763613c26e1e1e540
hajeu <s22072@gsm.hs.kr>
2023-04-05 10:28:08 +0900

    add AuthDetailsService 생성

--- src/main/java/com/themoment/officialgsm/global/exception/ErrorCode.java
+++ src/main/java/com/themoment/officialgsm/global/exception/ErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    UNKNOWN_ERROR("알 수 없는 에러입니다.", 500);
+    UNKNOWN_ERROR("알 수 없는 에러입니다.", 500),
+    USER_NOT_FOUND("유저를 찾을 수 없습니다.", 404);
 
     private final String message;
     private final Integer status;
---
+++ src/main/java/com/themoment/officialgsm/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,23 @@
+package com.themoment.officialgsm.global.security.auth;
+
+import com.themoment.officialgsm.domain.Admin.repository.UserRepository;
+import com.themoment.officialgsm.global.exception.CustomException;
+import com.themoment.officialgsm.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userid) throws UsernameNotFoundException {
+        return userRepository.findUserByUserId(userid)
+                .map(AuthDetails::new)
+                .orElseThrow(()-> new CustomException(ErrorCode.USER_NOT_FOUND));
+    }
+}
