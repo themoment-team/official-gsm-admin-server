4a9515ea2d5803486f0d3b5176bc53011ea90cc9
jangwooooo <s22018@gsm.hs.kr>
2023-04-05 13:32:33 +0900

    Add ErrorResponse

---
+++ src/main/java/com/themoment/officialgsm/global/exception/ErrorResponse.java
@@ -0,0 +1,28 @@
+package com.themoment.officialgsm.global.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Builder
+@Getter
+public class ErrorResponse {
+
+    private final String formatNow = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd / HH : mm : ss "));
+    private final int status;
+    private final String message;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.builder()
+                        .status(errorCode.getStatus())
+                        .message(errorCode.getDetail())
+                        .build()
+                );
+    }
+
+}
