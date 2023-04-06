b679f1853a22bb178106c38c19c081bdf24442a3
jangwooooo <s22018@gsm.hs.kr>
2023-04-06 09:25:09 +0900

    Add FILE_UPLOAD_ERROR, WRONG_INPUT_FILE

--- src/main/java/com/themoment/officialgsm/global/exception/ErrorCode.java
+++ src/main/java/com/themoment/officialgsm/global/exception/ErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum ErrorCode {
 
     UNKNOWN_ERROR(500, "알 수 없는 에러입니다."),
-    USER_NOT_FOUND(404, "존재하지 않는 유저입니다.");
+    USER_NOT_FOUND(404, "존재하지 않는 유저입니다."),
+    FILE_UPLOAD_ERROR(400, "파일 업로드 에러"),
+    WRONG_INPUT_FILE(400, "잘못된 파일입니다.");
 
     private final int status;
     private final String detail;
