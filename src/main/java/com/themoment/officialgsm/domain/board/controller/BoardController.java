e72a697820fc3a76c7279f6a36367ff0bed40f51
jangwooooo <s22018@gsm.hs.kr>
2023-04-06 20:01:31 +0900

    Add boardController

--- src/main/java/com/themoment/officialgsm/domain/board/controller/BoardController.java
+++ src/main/java/com/themoment/officialgsm/domain/board/controller/BoardController.java
@@ -1,2 +1,27 @@
-package com.themoment.officialgsm.domain.board.controller;public class BoardController {
+package com.themoment.officialgsm.domain.board.controller;
+
+import com.themoment.officialgsm.domain.board.dto.request.AddPostRequest;
+import com.themoment.officialgsm.domain.board.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping
+    public ResponseEntity<Void> postAdd(@RequestPart("content") AddPostRequest addPostRequest, @RequestPart(value = "file", required = false) List<MultipartFile> multipartFiles) {
+        boardService.addPost(addPostRequest, multipartFiles);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
