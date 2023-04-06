42788aa29a7e8917cbeab800bea7530786ef8d5d
jangwooooo <s22018@gsm.hs.kr>
2023-04-06 20:58:04 +0900

    Add POST_NOT_FOUND

--- src/main/java/com/themoment/officialgsm/global/exception/ErrorCode.java
+++ src/main/java/com/themoment/officialgsm/global/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
     UNKNOWN_ERROR(500, "알 수 없는 에러입니다."),
     USER_NOT_FOUND(404, "존재하지 않는 유저입니다."),
     FILE_UPLOAD_ERROR(400, "파일 업로드 에러"),
-    WRONG_INPUT_FILE(400, "잘못된 파일입니다.");
+    WRONG_INPUT_FILE(400, "잘못된 파일입니다."),
+    POST_NOT_FOUND(404, "존재하지 않는 게시글입니다.");
 
     private final int status;
     private final String detail;
