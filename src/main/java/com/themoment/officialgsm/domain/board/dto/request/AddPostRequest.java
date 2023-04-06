780ac9897fe5d8971b89630e6776a3cebea9278f
jangwooooo <s22018@gsm.hs.kr>
2023-04-06 19:59:57 +0900

    Add AddPostRequest

--- src/main/java/com/themoment/officialgsm/domain/board/dto/request/AddPostRequest.java
+++ src/main/java/com/themoment/officialgsm/domain/board/dto/request/AddPostRequest.java
@@ -1,2 +1,14 @@
-package com.themoment.officialgsm.domain.board.dto.request;public class AddPostRequest {
+package com.themoment.officialgsm.domain.board.dto.request;
+
+import com.themoment.officialgsm.domain.board.entity.post.Category;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AddPostRequest {
+
+    private String postTitle;
+    private String postContent;
+    private Category category;
 }
