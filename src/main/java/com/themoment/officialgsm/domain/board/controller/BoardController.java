d62dfd8294f17d5d4dd33ba7ed1db9fecc08d833
jangwooooo <s22018@gsm.hs.kr>
2023-04-06 20:24:37 +0900

    Add 임시 url매핑

--- src/main/java/com/themoment/officialgsm/domain/board/controller/BoardController.java
+++ src/main/java/com/themoment/officialgsm/domain/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import com.themoment.officialgsm.domain.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @PostMapping
+    @PostMapping("/add")
     public ResponseEntity<Void> postAdd(@RequestPart("content") AddPostRequest addPostRequest, @RequestPart(value = "file", required = false) List<MultipartFile> multipartFiles) {
         boardService.addPost(addPostRequest, multipartFiles);
         return new ResponseEntity<>(HttpStatus.CREATED);
