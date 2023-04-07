b0b0a97ce28805c5dd61d03622f6a3bef71c1ad9
jangwooooo <s22018@gsm.hs.kr>
2023-04-07 10:18:49 +0900

    Add deleteByFileUrl method

--- src/main/java/com/themoment/officialgsm/domain/board/repository/FileRepository.java
+++ src/main/java/com/themoment/officialgsm/domain/board/repository/FileRepository.java
@@ -4,4 +4,6 @@ import com.themoment.officialgsm.domain.board.entity.file.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository extends JpaRepository<File, Long> {
+
+    void deleteByFileUrl(String fileUrl);
 }
