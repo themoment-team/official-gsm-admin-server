9e7e8777b53d2655188a8d80fff38819a02bd96f
hajeu <s22072@gsm.hs.kr>
2023-04-07 20:13:29 +0900

    add generateToken 메서드

--- src/main/java/com/themoment/officialgsm/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/themoment/officialgsm/global/security/jwt/JwtTokenProvider.java
@@ -1,14 +1,49 @@
 package com.themoment.officialgsm.global.security.jwt;
 
+import com.themoment.officialgsm.global.security.jwt.properties.JwtProperties;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Getter
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class JwtTokenProvider {
+    private final JwtProperties jwtProperties;
 
+    @AllArgsConstructor
+    public enum TokenType{
+        ACCESS_TOKEN,
+        REFRESH_TOKEN
+    }
+
+    private Key getSignInKey(String secretKey){
+        byte[] bytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    private String generateToken(String userId, String type, String secret, long expiredTime){
+        final Claims claims = Jwts.claims();
+        claims.put("userId", userId);
+        claims.put("type", type);
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()))
+                .signWith(getSignInKey(secret), SignatureAlgorithm.ES256)
+                .compact();
+    }
 }
