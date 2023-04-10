74201f51cd6467bd602cd38af83a398fffbdd3e3
hajeu <s22072@gsm.hs.kr>
2023-04-10 19:43:28 +0900

    refactor schoolIp 외부에서 모르도록 수정

--- src/main/java/com/themoment/officialgsm/domain/Admin/service/UserService.java
+++ src/main/java/com/themoment/officialgsm/domain/Admin/service/UserService.java
@@ -1,8 +1,12 @@
 package com.themoment.officialgsm.domain.Admin.service;
 
+import com.themoment.officialgsm.domain.Admin.presentation.dto.request.SignInRequest;
 import com.themoment.officialgsm.domain.Admin.presentation.dto.request.SignUpRequest;
+import com.themoment.officialgsm.domain.Admin.presentation.dto.response.SignInResponse;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface UserService {
     void signUp(SignUpRequest signUpRequest, String ip);
+
+    SignInResponse signIn(SignInRequest signInRequest);
 }
--- src/main/java/com/themoment/officialgsm/domain/Admin/service/impl/UserServiceImpl.java
+++ src/main/java/com/themoment/officialgsm/domain/Admin/service/impl/UserServiceImpl.java
@@ -1,13 +1,17 @@
 package com.themoment.officialgsm.domain.Admin.service.impl;
 
 import com.themoment.officialgsm.domain.Admin.entity.User;
+import com.themoment.officialgsm.domain.Admin.presentation.dto.request.SignInRequest;
 import com.themoment.officialgsm.domain.Admin.presentation.dto.request.SignUpRequest;
+import com.themoment.officialgsm.domain.Admin.presentation.dto.response.SignInResponse;
 import com.themoment.officialgsm.domain.Admin.repository.UserRepository;
 import com.themoment.officialgsm.domain.Admin.service.UserService;
 import com.themoment.officialgsm.global.exception.CustomException;
 import com.themoment.officialgsm.global.exception.ErrorCode;
+import com.themoment.officialgsm.global.security.jwt.JwtTokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,10 +20,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
+    @Value("${ip}")
+    private final String schoolIp;
+
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void signUp(SignUpRequest signUpRequest, String ip) {
-        String schoolIp = "210.218.52.13";
         if (userRepository.existsByUserId(signUpRequest.getUserId())){
             throw new CustomException(ErrorCode.USERID_ALREADY_EXIST);
         }
@@ -32,5 +38,11 @@ public class UserServiceImpl implements UserService {
                 .userName(signUpRequest.getName())
                 .build();
         userRepository.save(user);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public SignInResponse signIn(SignInRequest signInRequest) {
+
     }
 }
