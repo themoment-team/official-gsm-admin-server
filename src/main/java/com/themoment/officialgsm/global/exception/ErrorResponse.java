76790b9a5329854a0b02ee3a4b4013cd7d1b755f
jangwooooo <s22018@gsm.hs.kr>
2023-04-11 11:19:52 +0900

    Fix message대신 detailMessage를 반환하도록 수정

--- src/main/java/com/themoment/officialgsm/global/exception/ErrorResponse.java
+++ src/main/java/com/themoment/officialgsm/global/exception/ErrorResponse.java
@@ -13,14 +13,14 @@ public class ErrorResponse {
 
     private final String formatNow = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd / HH : mm : ss "));
     private final int status;
-    private final String message;
+    private final String detailMessage;
 
-    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode) {
+    public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode errorCode, String detailMessage) {
         return ResponseEntity
                 .status(errorCode.getStatus())
                 .body(ErrorResponse.builder()
                         .status(errorCode.getStatus())
-                        .message(errorCode.getMessage())
+                        .detailMessage(detailMessage)
                         .build()
                 );
     }
