f02e4d834b0050a8bc4d65c2b39d4deebfb827ee
jangwooooo <s22018@gsm.hs.kr>
2023-04-12 11:18:57 +0900

    Fix errocode제거, httpstatus핃르 추가

--- src/main/java/com/themoment/officialgsm/global/exception/CustomException.java
+++ src/main/java/com/themoment/officialgsm/global/exception/CustomException.java
@@ -1,15 +1,16 @@
 package com.themoment.officialgsm.global.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class CustomException extends RuntimeException{
 
-    private final ErrorCode errorCode;
     private final String detailMessage;
+    private final HttpStatus httpStatus;
 
-    public CustomException(ErrorCode errorCode, String detailMessage) {
-        this.errorCode = errorCode;
+    public CustomException(String detailMessage, HttpStatus httpStatus) {
         this.detailMessage = detailMessage;
+        this.httpStatus = httpStatus;
     }
 }
