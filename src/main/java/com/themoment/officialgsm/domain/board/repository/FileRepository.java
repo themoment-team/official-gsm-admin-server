786f6945bfd0c842c7d9131e07c41a18cc211ada
jangwooooo <s22018@gsm.hs.kr>
2023-04-12 10:02:25 +0900

    Fix bulk delete를 처리하도록 변경

--- src/main/java/com/themoment/officialgsm/domain/board/repository/FileRepository.java
+++ src/main/java/com/themoment/officialgsm/domain/board/repository/FileRepository.java
@@ -2,8 +2,14 @@ package com.themoment.officialgsm.domain.board.repository;
 
 import com.themoment.officialgsm.domain.board.entity.file.File;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FileRepository extends JpaRepository<File, Long> {
 
-    void deleteByFileUrl(String fileUrl);
+    @Modifying
+    @Query("DELETE FROM File f WHERE f.fileUrl IN :fileUrls")
+    void deleteByFileUrls(List<String> fileUrls);
 }
