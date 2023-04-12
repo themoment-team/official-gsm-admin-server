f1dda1414039571fd2a4297c3a3bd7fba489c29b
jangwooooo <s22018@gsm.hs.kr>
2023-04-12 11:23:09 +0900

    Fix toResponseEntoty 인수 수정

--- src/main/java/com/themoment/officialgsm/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/themoment/officialgsm/global/exception/handler/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.themoment.officialgsm.global.exception.handler;
 
 import com.themoment.officialgsm.global.exception.CustomException;
-import com.themoment.officialgsm.global.exception.ErrorCode;
 import com.themoment.officialgsm.global.exception.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,14 +14,14 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
-        log.error("handleCustomException throw CustomException : {}", e.getErrorCode(), e);
-        return ErrorResponse.toResponseEntity(e.getErrorCode(), e.getDetailMessage());
+        log.error("handleCustomException throw CustomException : {}", e.getMessage(), e);
+        return ErrorResponse.toResponseEntity(e.getDetailMessage(), e.getHttpStatus());
     }
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleCustomException(Exception e) {
         log.error("UnknownExceptionHandler throw Exception : {}", e.getMessage(), e);
-        return ErrorResponse.toResponseEntity(ErrorCode.UNKNOWN_ERROR, "예외 처리되지 않은 에러가 발생하였습니다.");
+        return ErrorResponse.toResponseEntity("예외 처리되지 않은 에러가 발생하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
