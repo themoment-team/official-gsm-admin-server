9fdd9b9fd77ded85f1b5541f7ac5225663e51a3e
jangwooooo <s22018@gsm.hs.kr>
2023-04-12 20:36:06 +0900

    Rename AwsConfig -> AwsS3ServiceConfig

--- src/main/java/com/themoment/officialgsm/global/config/AwsS3ServiceConfig.java
+++ src/main/java/com/themoment/officialgsm/global/config/AwsS3ServiceConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AwsConfig {
+public class AwsS3ServiceConfig {
 
     @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
