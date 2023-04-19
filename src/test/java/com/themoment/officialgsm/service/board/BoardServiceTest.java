0e8dea4f6af50c28888d6a891378727486a8e7bb
jangwooooo <s22018@gsm.hs.kr>
2023-04-19 19:50:31 +0900

    Add 게시글 목록 조회 Transactional(readOnly=true)적용

---
+++ src/test/java/com/themoment/officialgsm/service/board/BoardServiceTest.java
@@ -0,0 +1,34 @@
+package com.themoment.officialgsm.domain.board.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void findPosts() {
+    }
+
+    @Test
+    void addPost() {
+    }
+
+    @Test
+    void modifyPost() {
+    }
+
+    @Test
+    void removePost() {
+    }
+}
