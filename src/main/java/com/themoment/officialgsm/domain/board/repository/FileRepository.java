5efc31631fb2a20789923e1025595309b75d4c34
jangwooooo <s22018@gsm.hs.kr>
2023-04-19 19:54:41 +0900

    Add Param어노테이션으로 fileUrls 명시

--- src/main/java/com/themoment/officialgsm/domain/board/repository/FileRepository.java
+++ src/main/java/com/themoment/officialgsm/domain/board/repository/FileRepository.java
@@ -4,6 +4,7 @@ import com.themoment.officialgsm.domain.board.entity.file.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,5 +12,5 @@ public interface FileRepository extends JpaRepository<File, String> {
 
     @Modifying
     @Query("DELETE FROM File f WHERE f.fileUrl IN :fileUrls")
-    void deleteByFileUrls(List<String> fileUrls);
+    void deleteByFileUrls(@Param("fileUrls") List<String> fileUrls);
 }
