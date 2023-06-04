87a29907d97a3647990ac132bfe95b17c359aade
hajeu <s22072@gsm.hs.kr>
2023-06-04 10:39:37 +0900

    add security config 설정 and corsConfigurationSource

--- src/main/java/com/themoment/officialgsm/global/security/SecurityConfig.java
+++ src/main/java/com/themoment/officialgsm/global/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import com.themoment.officialgsm.global.security.handler.CustomAuthenticationEnt
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,6 +17,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -47,8 +53,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
                         .requestMatchers("/main.html").permitAll()
                         .requestMatchers("/auth/token/reissue").permitAll()
+                        .requestMatchers("/auth/logout").permitAll()
+                        .requestMatchers("/api/post/**").hasAuthority("ADMIN")
                         .requestMatchers("/auth/unapproved/list").hasAuthority("ADMIN")
-                        .requestMatchers("/auth/approved/*").hasAuthority("ADMIN")
+                        .requestMatchers("/auth/approved/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated();
         httpSecurity
                 .sessionManagement()
@@ -73,5 +81,15 @@ public class SecurityConfig {
                 .userService(oAuthService);
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("")); // 추후 수정
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
