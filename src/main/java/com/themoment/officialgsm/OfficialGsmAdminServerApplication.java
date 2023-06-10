53afe450243a7d5e74a670479cf257129d46aa49
hajeu <s22072@gsm.hs.kr>
2023-06-10 20:07:08 +0900

    remove keystore and connector

--- src/main/java/com/themoment/officialgsm/OfficialGsmAdminServerApplication.java
+++ src/main/java/com/themoment/officialgsm/OfficialGsmAdminServerApplication.java
@@ -1,14 +1,7 @@
 package com.themoment.officialgsm;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -17,31 +10,6 @@ public class OfficialGsmAdminServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OfficialGsmAdminServerApplication.class, args);
-    }
-
-    @Bean
-    public ServletWebServerFactory serveltContainer(){
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
-        return tomcat;
-    }
-    private Connector createStandardConnector() {
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setSecure(false);
-        connector.setPort(9001);
-        connector.setRedirectPort(9000);
-        return connector;
     }
 
 }
