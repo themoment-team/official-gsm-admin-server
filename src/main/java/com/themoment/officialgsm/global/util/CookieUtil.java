8edc4b97c6f7017a0affadc0fc7f2ef70fdc2282
hajeu <s22072@gsm.hs.kr>
2023-06-12 20:27:02 +0900

    add cookie domain

--- src/main/java/com/themoment/officialgsm/global/util/CookieUtil.java
+++ src/main/java/com/themoment/officialgsm/global/util/CookieUtil.java
@@ -3,17 +3,25 @@ package com.themoment.officialgsm.global.util;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@AllArgsConstructor
+@Getter
 public class CookieUtil {
+    @Value("${cookie-domain}")
+    private static String cookieDomain;
+
     public static void addTokenCookie(HttpServletResponse response, String name, String value, Long maxAge, boolean httpOnly){
         Cookie cookie = new Cookie(name, value);
         cookie.setMaxAge(Math.toIntExact(maxAge)/1000);
         cookie.setHttpOnly(httpOnly);
-        cookie.setDomain("");
+        cookie.setDomain(cookieDomain);
         cookie.setPath("/");
         response.addCookie(cookie);
     }
