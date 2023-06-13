9b6abcb5141e0be901df6b6bdb50ad3f769a91a3
jangwooooo <s22018@gsm.hs.kr>
2023-06-13 18:23:19 +0900

    Add /api to authAPI

--- src/main/java/com/themoment/officialgsm/domain/auth/controller/UserController.java
+++ src/main/java/com/themoment/officialgsm/domain/auth/controller/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
