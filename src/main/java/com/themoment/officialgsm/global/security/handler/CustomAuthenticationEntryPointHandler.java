9d9b3b77845adb35b0d0a15a82ceec01b50dd5bd
hajeu <s22072@gsm.hs.kr>
2023-06-13 20:53:23 +0900

    add oauth 실패시 리다이렉트

--- src/main/java/com/themoment/officialgsm/global/security/handler/CustomAuthenticationEntryPointHandler.java
+++ src/main/java/com/themoment/officialgsm/global/security/handler/CustomAuthenticationEntryPointHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -13,9 +14,14 @@ import java.io.IOException;
 @Slf4j
 @Component
 public class CustomAuthenticationEntryPointHandler implements AuthenticationEntryPoint {
+
+    @Value("${oauth-fail-redirect}")
+    private String oauthFailRedirectUrl;
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         log.warn("AUTHENTICATION_ENTRYPOINT");
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.sendRedirect(oauthFailRedirectUrl);
     }
 }
