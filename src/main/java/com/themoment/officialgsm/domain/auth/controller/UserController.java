f75112180e2ab346ca46d2daf8edf87b8bf7b9f1
jangwooooo <s22018@gsm.hs.kr>
2023-06-13 22:32:37 +0900

    prettier

--- src/main/java/com/themoment/officialgsm/domain/auth/controller/UserController.java
+++ src/main/java/com/themoment/officialgsm/domain/auth/controller/UserController.java
@@ -7,9 +7,9 @@ import com.themoment.officialgsm.domain.auth.service.GrantorService;
 import com.themoment.officialgsm.domain.auth.service.UserService;
 import com.themoment.officialgsm.global.util.ConstantsUtil;
 import com.themoment.officialgsm.global.util.CookieUtil;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -28,52 +28,93 @@ public class UserController {
     private final GrantorService grantorService;
 
     @PatchMapping("/username")
-    @Operation(summary = "이름 등록 요청", description = "oauth로그인 후 이름을 등록하는 api", tags = {"User Controller"})
-    public ResponseEntity<Void> nameSet(@Valid @RequestBody UserNameRequest request){
+    @Operation(
+            summary = "이름 등록 요청",
+            description = "oauth로그인 후 이름을 등록하는 api",
+            tags = {"User Controller"}
+    )
+    public ResponseEntity<Void> nameSet(
+            @Valid @RequestBody UserNameRequest request
+    ){
         userService.nameSetExecute(request);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/userinfo")
-    @Operation(summary = "회원정보 조회 요청", description = "회원정보를 조회하는 api", tags = {"User Controller"})
+    @Operation(
+            summary = "회원정보 조회 요청",
+            description = "회원정보를 조회하는 api",
+            tags = {"User Controller"}
+    )
     public ResponseEntity<UserInfoResponse> userInfo(){
         UserInfoResponse userinfo = userService.userInfoExecute();
         return new ResponseEntity<>(userinfo, HttpStatus.OK);
     }
 
     @DeleteMapping("/logout")
-    @Operation(summary = "로그아웃 요청", description = "로그아웃하는 api", tags = {"User Controller"})
-    public ResponseEntity<Void> logout(HttpServletRequest request){
+    @Operation(
+            summary = "로그아웃 요청",
+            description = "로그아웃하는 api",
+            tags = {"User Controller"}
+    )
+    public ResponseEntity<Void> logout(
+            HttpServletRequest request
+    ){
         String accessToken = CookieUtil.getCookieValue(request, ConstantsUtil.accessToken);
         userService.logout(accessToken);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @GetMapping("/token/reissue")
-    @Operation(summary = "토큰 재발급 요청", description = "토큰을 재발급하는 api", tags = {"User Controller"})
-    public ResponseEntity<Void> tokenReissue(HttpServletRequest request, HttpServletResponse response){
+    @Operation(
+            summary = "토큰 재발급 요청",
+            description = "토큰을 재발급하는 api",
+            tags = {"User Controller"}
+    )
+    public ResponseEntity<Void> tokenReissue(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ){
         String token = CookieUtil.getCookieValue(request, ConstantsUtil.refreshToken);
         userService.tokenReissue(token, response);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/unapproved/list")
-    @Operation(summary = "승인대기 회원정보 조회 요청", description = "승인대기중인 회원정보를 조회하는 api", tags = {"User Controller"})
+    @Operation(
+            summary = "승인대기 회원정보 조회 요청",
+            description = "승인대기중인 회원정보를 조회하는 api",
+            tags = {"User Controller"}
+    )
     public ResponseEntity<List<UnapprovedUserResponse>> unapprovedList(){
         List<UnapprovedUserResponse> list = grantorService.unapprovedListExecute();
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @PatchMapping("/approved/{userSeq}")
-    @Operation(summary = "회원을 승인하는 요청", description = "승인대기중인 회원을 승인하는 api", tags = {"User Controller"})
-    public ResponseEntity<Void> approved(@PathVariable Long userSeq){
+    @Operation(
+            summary = "회원을 승인하는 요청",
+            description = "승인대기중인 회원을 승인하는 api",
+            tags = {"User Controller"}
+    )
+    public ResponseEntity<Void> approved(
+            @Parameter(name = "userSeq", description = "user의 seq값", in = ParameterIn.PATH)
+            @PathVariable Long userSeq
+    ){
         grantorService.approvedExecute(userSeq);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @DeleteMapping("/approved/{userSeq}")
-    @Operation(summary = "회원 승인을 거절하는 요청", description = "승인대기중인 회원을 승인 거절하는 api", tags = {"User Controller"})
-    public ResponseEntity<Void> refuseApproved(@PathVariable Long userSeq){
+    @Operation(
+            summary = "회원 승인을 거절하는 요청",
+            description = "승인대기중인 회원을 승인 거절하는 api",
+            tags = {"User Controller"}
+    )
+    public ResponseEntity<Void> refuseApproved(
+            @Parameter(name = "userSeq", description = "user의 seq값", in = ParameterIn.PATH)
+            @PathVariable Long userSeq
+    ){
         grantorService.refuseApprovedExecute(userSeq);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
--- src/main/java/com/themoment/officialgsm/domain/board/controller/BoardController.java
+++ src/main/java/com/themoment/officialgsm/domain/board/controller/BoardController.java
@@ -5,7 +5,8 @@ import com.themoment.officialgsm.domain.board.dto.request.ModifyPostRequest;
 import com.themoment.officialgsm.domain.board.service.BoardService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,22 +23,45 @@ public class BoardController {
     private final BoardService boardService;
 
     @PostMapping
-    @Operation(summary = "게시물 등록 요청", description = "게시물을 등록하는 api", tags = {"Board Controller"})
-    public ResponseEntity<Void> postAdd(@RequestPart("content") AddPostRequest addPostRequest, @RequestPart(value = "file", required = false) List<MultipartFile> files) {
+    @Operation(
+            summary = "게시물 등록 요청",
+            description = "게시물을 등록하는 api",
+            tags = {"Board Controller"}
+    )
+    public ResponseEntity<Void> postAdd(
+            @RequestPart("content") AddPostRequest addPostRequest,
+            @RequestPart(value = "file", required = false) List<MultipartFile> files
+    ) {
         boardService.addPost(addPostRequest, files);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PatchMapping("/{postSeq}")
-    @Operation(summary = "게시물 수정 요청", description = "게시물을 수정하는 api", tags = {"Board Controller"})
-    public ResponseEntity<Void> postModify(@PathVariable Long postSeq, @RequestPart("content") ModifyPostRequest modifyPostRequest, @RequestPart(value = "file", required = false) List<MultipartFile> files) {
+    @Operation(
+            summary = "게시물 수정 요청",
+            description = "게시물을 수정하는 api",
+            tags = {"Board Controller"}
+    )
+    public ResponseEntity<Void> postModify(
+            @Parameter(name = "postSeq", description = "post의 seq값", in = ParameterIn.PATH)
+            @PathVariable Long postSeq,
+            @RequestPart("content") ModifyPostRequest modifyPostRequest,
+            @RequestPart(value = "file", required = false) List<MultipartFile> files
+    ) {
         boardService.modifyPost(postSeq, modifyPostRequest, files);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/{postSeq}")
-    @Operation(summary = "게시물 삭제 요청", description = "게시물을 삭제하는 api", tags = {"Board Controller"})
-    public ResponseEntity<Void> postDelete(@PathVariable Long postSeq) {
+    @Operation(
+            summary = "게시물 삭제 요청",
+            description = "게시물을 삭제하는 api",
+            tags = {"Board Controller"}
+    )
+    public ResponseEntity<Void> postDelete(
+            @Parameter(name = "postSeq", description = "post의 seq값", in = ParameterIn.PATH)
+            @PathVariable Long postSeq
+    ) {
         boardService.removePost(postSeq);
         return new ResponseEntity<>(HttpStatus.OK);
     }
