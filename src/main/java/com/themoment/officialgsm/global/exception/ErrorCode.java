f6c0987576cdfdb8d044a34782b12f96cdf486ce
hajeu <s22072@gsm.hs.kr>
2023-07-02 02:44:23 +0900

    add ExceptionHandlerFilter 생성 and filter단 예외 catch

--- src/main/java/com/themoment/officialgsm/global/exception/ErrorCode.java
+++ src/main/java/com/themoment/officialgsm/global/exception/ErrorCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    UNKNOWN_ERROR("알 수 없는 에러입니다.", 500),
     INVALID_TOKEN("유효하지 않은 토큰입니다.",401);
 
     private final String message;
---
+++ src/main/java/com/themoment/officialgsm/global/exception/handler/ExceptionHandlerFilter.java
@@ -0,0 +1,51 @@
+package com.themoment.officialgsm.global.exception.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.themoment.officialgsm.global.exception.ErrorCode;
+import com.themoment.officialgsm.global.exception.ErrorResponse;
+import com.themoment.officialgsm.global.exception.handler.exceptionCollection.InvalidTokenException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.entity.ContentType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (InvalidTokenException e){
+            errorMessageResponse(response, e.getErrorCode());
+        } catch (Exception e){
+            log.error("알 수 없는 에러 발생", e);
+            errorMessageResponse(response, ErrorCode.UNKNOWN_ERROR);
+        }
+    }
+
+    private void errorMessageResponse(HttpServletResponse response, ErrorCode errorCode){
+        response.setContentType(ContentType.APPLICATION_JSON.getMimeType());
+        response.setStatus(errorCode.getStatus());
+
+        try {
+            ErrorResponse errorResponse = new ErrorResponse(errorCode);
+            String errorResponseEntityToJson = objectMapper.writeValueAsString(errorResponse);
+            response.getWriter().write(errorResponseEntityToJson);
+        } catch (IOException e) {
+            log.error("Filter에서 ErrorResponse Json 변환 실패", e);
+            throw new RuntimeException(e);
+        }
+
+    }
+}
