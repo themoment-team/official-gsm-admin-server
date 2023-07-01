2f30387c051e71d4efdae93ef22edf05ae968d4a
hajeu <s22072@gsm.hs.kr>
2023-07-02 03:19:07 +0900

    fix error message 영어로 수정

--- src/main/java/com/themoment/officialgsm/global/exception/ErrorCode.java
+++ src/main/java/com/themoment/officialgsm/global/exception/ErrorCode.java
@@ -6,11 +6,11 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    INVALID_REFRESH_TOKEN("유효하지 않은 리프레시 토큰입니다. ", 400),
-    REFRESH_TOKEN_EXPIRED("리프레시 토큰이 만료되었습니다.", 400),
-    TOKEN_EXPIRED("토큰이 만료되었습니다.", 401),
-    UNKNOWN_ERROR("알 수 없는 에러입니다.", 500),
-    INVALID_TOKEN("유효하지 않은 토큰입니다.", 401);
+    INVALID_REFRESH_TOKEN("Invalid Refresh Token", 400),
+    REFRESH_TOKEN_EXPIRED("Refresh Token Expired", 400),
+    TOKEN_EXPIRED("Token Expired", 401),
+    UNKNOWN_ERROR("Unknown Error", 500),
+    INVALID_TOKEN("Invalid Token", 401);
 
     private final String message;
     private final int status;
