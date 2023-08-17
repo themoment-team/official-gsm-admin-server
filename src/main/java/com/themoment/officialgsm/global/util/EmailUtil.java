bff0f226bf70e7a3ca29537eb40e474b8551bce4
hajeu <s22072@gsm.hs.kr>
2023-08-17 16:23:02 +0900

    add comment

--- src/main/java/com/themoment/officialgsm/global/util/EmailUtil.java
+++ src/main/java/com/themoment/officialgsm/global/util/EmailUtil.java
@@ -12,11 +12,13 @@ public class EmailUtil {
     public String getOauthEmailDomain(String email) {
         int index = email.indexOf("@");
 
+        /*
         String emailId = email.substring(0, index);
 
         if (emailId.matches(emailIdRegex)){
             throw new IllegalArgumentException("학생은 로그인할 수 없습니다.");
         }
+        */
 
         return email.substring(index + 1);
     }
