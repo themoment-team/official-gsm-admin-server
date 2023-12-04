bf838ab0a26f35a12beb8dc056bebf2023946997
jangwooooo <s22018@gsm.hs.kr>
2023-12-04 14:05:15 +0900

    create popup repository

---
+++ src/main/java/com/themoment/officialgsm/domain/popup/repository/PopupRepository.java
@@ -0,0 +1,7 @@
+package com.themoment.officialgsm.domain.popup.repository;
+
+import com.themoment.officialgsm.domain.popup.entity.Popup;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PopupRepository extends JpaRepository<Popup, Long> {
+}
